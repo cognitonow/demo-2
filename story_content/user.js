498b003c3ca5bc333f7b4d34323bf3c185dea768
cognitonow <cognito.learning.now@gmail.com>
2020-05-12 13:16:06 +0200

    qwe
    qwe

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6p6LwGj3zN6":
+        Script1();
+        break;
+      case "600fnZ5MNE2":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  SetStatus("completed")
+}
+
+function Script2()
+{
+  SetStatus("incomplete")
+}
+
